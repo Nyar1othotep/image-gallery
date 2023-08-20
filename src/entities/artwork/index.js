a659c59b363ac82d6d261d587172336b0a228707
Maxim <freelancerart@yandex.ru>
2023-08-20 15:40:03 +0400

    feat: add comment and move lib from features to entities

--- src/entities/artwork/index.js
+++ src/entities/artwork/index.js
@@ -3,3 +3,4 @@ export { ArtworkDetails } from "./ui/ArtworkDetails/ArtworkDetails";
 export { ArtworkFull } from "./ui/ArtworkFull/ArtworkFull";
 export { ArtworkLink } from "./ui/ArtworkLink/ArtworkLink";
 export { useGetArtworksQuery, useGetArtworkQuery } from "./api/artworkApi";
+export { convertToArray } from "./lib/convertToArray";
