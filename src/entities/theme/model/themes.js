c279caacac843077c5ba700e4e7b18de7c2977dd
Maxim <freelancerart@yandex.ru>
2023-09-14 15:10:16 +0400

    refactor: delete remark

--- src/entities/theme/model/themes.js
+++ src/entities/theme/model/themes.js
@@ -1,10 +1,3 @@
-/**
- * @remark
- *
- * I believe that this could fall under the concept of "Infrastructure Data."
- * Therefore, I've moved this into a separate file.
- */
-
 export const themes = {
   dark: "dark",
   light: "light",
